3db2e049c07ba92e562266e7a6ed3b2b105655a5
qasimroy <qasimroy5234@gmail.com>
2023-12-07 17:59:04 +0500

    config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import VitePluginWindicss from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginWindicss()],
+  plugins: [react()],
 })
