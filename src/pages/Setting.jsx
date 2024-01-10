c20073e9254515671a1855a0753db971c5ba9b7c
qasimroy <qasimroy5234@gmail.com>
2024-01-10 19:10:13 +0500

    setting edit

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import { Navbar } from "../pages";
+import { Card, Navbar } from "../pages";
+import { AiOutlineSetting } from "react-icons/ai";
 
 export const Setting = () => {
     useEffect(() => {
@@ -8,6 +9,19 @@ export const Setting = () => {
     return (
         <div className="w-full h-screen">
             <Navbar title="Settings" />
+            <div className="p-12">
+                <Card width="w-72">
+                    <div className="w-full">
+                        <div className="flex items-center gap-2">
+                            <AiOutlineSetting />
+                            <h3 className="font-bold">General</h3>
+                        </div>
+                        <div className="p-2">
+                            <p>some text here</p>
+                        </div>
+                    </div>
+                </Card>
+            </div>
         </div>
     );
 };
